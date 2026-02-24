92482112eeb64ec82690d2930e1bb60c8c8ad70d
Saviam <saviambruno@gmail.com>
2026-02-24 14:35:12 -0300

    feat: configurar rotas e criar paginas base da aplicacao

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,33 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'marketplace',
+      component: () => import('@/modules/trades/pages/MarketplacePage.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/modules/auth/pages/LoginPage.vue')
+    },
+    {
+      path: '/registro',
+      name: 'registro',
+      component: () => import('@/modules/auth/pages/RegisterPage.vue')
+    },
+    {
+      path: '/minhas-cartas',
+      name: 'minhas-cartas',
+      component: () => import('@/modules/cards/pages/MyCardsPage.vue')
+    },
+    {
+      path: '/nova-troca',
+      name: 'nova-troca',
+      component: () => import('@/modules/trades/pages/CreateTradePage.vue')
+    }
+  ]
 })
 
 export default router
