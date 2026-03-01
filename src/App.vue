7efef724ae84d341b57c03a56235e8e5fb5eec66
Saviam <saviambruno@gmail.com>
2026-03-01 17:26:02 -0300

    style: padding no ConfirmDialog (modal de confirmação)
    - App.vue: unificar CSS responsivo para Toast e ConfirmDialog

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view />
-  <PToast position="top-center"/>
+  <PToast position="top-center" />
   <ConfirmDialog></ConfirmDialog>
 </template>
 
@@ -12,6 +12,7 @@ body {
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 .p-toast {
   left: 50% !important;
   transform: translateX(-50%) !important;
@@ -20,11 +21,23 @@ body {
   max-width: 400px !important;
 }
 
-/* Ajuste para desktop */
+/* ConfirmDialog com padding no mobile */
+.p-dialog.p-confirmdialog {
+  width: calc(100% - 32px) !important;
+  max-width: 400px !important;
+  margin: 16px !important;
+}
+
+/* Ajustes para desktop */
 @media (min-width: 768px) {
   .p-toast {
     width: auto !important;
     max-width: 400px !important;
+  }
+  
+  .p-dialog.p-confirmdialog {
+    width: auto !important;
+    margin: 0 auto !important;
   }
 }
 </style>
